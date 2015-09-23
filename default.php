267a34d5299ecdfa6aa39c41423a55d79843adf2
jetiikad <jetiikad@hotmail.com>
2015-09-23 12:21:33 -0500

    The default default.php

---
+++ default.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+<title>A05_SessionColors</title>
+</head>
+
+<body bgcolor="black" 
+      text="green" 
+      link="yellow" 
+      vlink="red" 
+      alink="cyan">
+
+ 
+
+<center>
+<h1>Bill's Home Page</h1>
+<p><img border="0" src="http://www.mugshots.org/misc/bill-gates.jpg"></p>
+<h2>My Three Favorite Links</h2>
+<ul>
+  <li>
+    <p style="line-height: 150%"><a href="http://en.wikipedia.org/wiki/Bill_Gates">Bill Gates Net Worth Page</a></li>
+  <li>
+    <p style="line-height: 150%"><a href="http://www.usdoj.gov/atr/cases/ms_index.htm">United States v. Microsoft</a></li>
+  <li>
+    <p style="line-height: 150%"><a href="http://www.microsoft.com">Microsoft Home Page</a>&nbsp;</li>
+</ul>
+
+
+</center>
+
+<h3 align="center">Click <a href="optionsForm.php">HERE</a> to change display options.</h3>
+
+<p>&nbsp;</p>
+
+</body>
+
+</html>
