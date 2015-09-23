da36c2b097a9a1ca28c10977c52dfbb16dfc7496
jetiikad <jetiikad@hotmail.com>
2015-09-23 12:22:40 -0500

    The default changeOptions.php

---
+++ changeOptions.php
@@ -0,0 +1,25 @@
+  Your Session Has Been Updated.
+<html>
+
+<head>
+
+<title>ChangeOptions</title>
+</head>
+
+<body>
+
+<p>A05_SessionColors/changeOptions.php</p>
+<center>
+
+
+
+<p><a href="default.php">Return to Main Page</a></p>
+
+
+<p>&nbsp;</p>
+
+
+</center>
+</body>
+
+</html>
